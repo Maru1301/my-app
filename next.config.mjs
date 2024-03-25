f168ae28aae7d2204648f976e9922419a2b4208c
MaruLin <how7633818@gmail.com>
2024-03-25 14:09:46 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    assetPrefix: 'https://maru1301.github.io/my-app/'
   };
 
 export default nextConfig;

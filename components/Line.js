506bee5b91745c8227df3a93d651bb5ca722f78e
MaruLin <how7633818@gmail.com>
2024-03-25 15:33:41 +0800

    Update

---
+++ components/Line.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Line, Transformer } from 'react-konva';
+
+const ResizableLine = ({ lineProps, isSelected, onSelect, onChange }) => {
+    const lineRef = React.useRef();
+    const trRef = React.useRef();
+  
+    React.useEffect(() => {
+      if (isSelected) {
+        // we need to attach transformer manually
+        trRef.current.nodes([lineRef.current]);
+        trRef.current.getLayer().batchDraw();
+      }
+    }, [isSelected]);
+  
+    return (
+        <>
+            <React.Fragment>
+                <Line
+                    id={lineProps.id}
+                    points={lineProps.points}
+                    stroke="black"
+                    strokeWidth={5}
+                    tension={0.5}
+                    lineCap="round"
+                    lineJoin="round"
+                    globalCompositeOperation={
+                        lineProps.tool === 'eraser' ? 'destination-out' : 'source-over'
+                    }
+                    draggable
+                    onClick={onSelect}
+                    onTap={onSelect}
+                    ref={lineRef}
+                    {...lineProps}
+                    onDragEnd={(e) => {
+                        onChange({
+                        ...lineProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                        });
+                    }}
+                    onTransformEnd={(e) => {
+                        // transformer is changing scale of the node
+                        // and NOT its width or height
+                        // but in the store we have only width and height
+                        // to match the data better we will reset scale on transform end
+                        const node = lineRef.current;
+                        console.log(node)
+                        const scaleX = node.scaleX();
+                        const scaleY = node.scaleY();
+            
+                        // we will reset it back
+                        node.scaleX(1);
+                        node.scaleY(1);
+                        onChange({
+                        ...lineProps,
+                        //x: node.x(),
+                        //y: node.y(),
+                        strokeWidth: node.attrs.strokeWidth * scaleX,
+                        // set minimal value
+                        });
+                    }}
+                />
+                {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    flipEnabled={false}
+                    boundBoxFunc={(oldBox, newBox) => {
+                    // limit resize
+                    if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
+                        return oldBox;
+                    }
+                    return newBox;
+                    }}
+                />
+                )}
+            </React.Fragment>
+        </>
+    );
+};
+
+export default ResizableLine;
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    assetPrefix: 'https://maru1301.github.io/my-app/',
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' },
-        '/Mainpage': { page: '/Mainpage' }
-      }
-    },
-  };
+const nextConfig = {};
 
 export default nextConfig;

ce4048706917f3e8f98c6eeada0aee638e6eca89
MaruLin <how7633818@gmail.com>
2024-03-30 16:43:37 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: "docs",
     assetPrefix: 'https://maru1301.github.io/my-app/',
     basePath: '/my-app',
 };

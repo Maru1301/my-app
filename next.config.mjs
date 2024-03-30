e93706e99d70c1fcfe801834b4904cc0f49654b4
MaruLin <how7633818@gmail.com>
2024-03-30 20:24:10 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: 'export',
     distDir: "docs",
-    assetPrefix: 'https://maru1301.github.io/my-app/docs/',
-    basePath: '/my-app',
+    assetPrefix: 'https://maru1301.github.io/my-app',
+    basePath: '/my-app/docs',
 };
 
 export default nextConfig;
